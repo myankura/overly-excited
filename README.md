b373cede0384426f73d1016bb301c494f1562954
Michael Yankura <myankura210@gmail.com>
2019-03-01 13:09:19 -0600

    Push up Some words are more exciting than others to github

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Practice: Stacking Words
+The learning objects of this exercise is to use iteration and string concatenation
 Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
 
 Example output:
@@ -25,3 +27,36 @@ The walrus danced through the trees in the light of
 The walrus danced through the trees in the light of the
 
 The walrus danced through the trees in the light of the moon
+
+Practice: Some words are more exciting than others
+The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one operation when a certain conditions is true, and a different operation if that condition is false.
+
+Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
+
+If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
+
+Example output:
+
+The
+
+The walrus
+
+The walrus danced!
+
+The walrus danced! through
+
+The walrus danced! through the
+
+The walrus danced! through the trees!
+
+The walrus danced! through the trees! in
+
+The walrus danced! through the trees! in the
+
+The walrus danced! through the trees! in the light!
+
+The walrus danced! through the trees! in the light! of
+
+The walrus danced! through the trees! in the light! of the
+
+The walrus danced! through the trees! in the light! of the moon!
--- overly-excited.js
+++ overly-excited.js
@@ -1,28 +1,36 @@
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement(theWordArray) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
-    for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        
-        //This just made a list complete list of all items 
-        // buildMeUp = theWordArray[i];
+    // for (let i = 0; i < theWordArray.length; i++) {
+    // Concatenate the new word onto buildMeUp
 
-        //This created an infinite loop
-        // let buildMeUp = theWordArray.length ++;
-        
-        //This will concatenate each index value of an array after another.
-        buildMeUp += theWordArray[i] + " ";
-        // Print buildMeUp to the console
+    //This just made a list complete list of all items 
+    // buildMeUp = theWordArray[i];
+
+    //This created an infinite loop, don't ever do this again.
+    // let buildMeUp = theWordArray.length ++;
+
+    //This was the solution to Practice: Stacking Words. This will concatenate each index value of an array after another.
+    // buildMeUp += theWordArray[i] + " ";
+
+    //This is the solution to Some words are more exciting than others
+    for (let i = 0; i < theWordArray.length; i++) {
+        if ((i + 1) % 3 === 0) {
+            buildMeUp += theWordArray[i] + "!";
+        } else {
+            buildMeUp += theWordArray[i] + " ";            
+        }
+        //Do not put console.log in the if else statement for this exercise, it will cause the moon to disapear!
         console.log(buildMeUp);
     }
 
