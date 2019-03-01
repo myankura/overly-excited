d0ef82434e7216654ad593702166b1a820c6b4d3
Michael Yankura <myankura210@gmail.com>
2019-03-01 13:29:13 -0600

    push Some words are more exciting than others

--- overly-excited.js
+++ overly-excited.js
@@ -12,27 +12,24 @@ function addExcitement(theWordArray) {
     let buildMeUp = ""
 
     // for (let i = 0; i < theWordArray.length; i++) {
-    // Concatenate the new word onto buildMeUp
-
     //This just made a list complete list of all items 
     // buildMeUp = theWordArray[i];
-
     //This created an infinite loop, don't ever do this again.
     // let buildMeUp = theWordArray.length ++;
-
     //This was the solution to Practice: Stacking Words. This will concatenate each index value of an array after another.
     // buildMeUp += theWordArray[i] + " ";
+    //}
 
     //This is the solution to Some words are more exciting than others
-    for (let i = 0; i < theWordArray.length; i++) {
-        if ((i + 1) % 3 === 0) {
-            buildMeUp += theWordArray[i] + "!";
-        } else {
-            buildMeUp += theWordArray[i] + " ";            
-        }
-        //Do not put console.log in the if else statement for this exercise, it will cause the moon to disapear!
-        console.log(buildMeUp);
-    }
+    // for (let i = 0; i < theWordArray.length; i++) {
+    //     if ((i + 1) % 3 === 0) {
+    //         buildMeUp += theWordArray[i] + "!";
+    //     } else {
+    //         buildMeUp += theWordArray[i] + " ";
+    //     }
+    //     //Do not put console.log in the if else statement for this exercise, it will cause the moon to disapear!
+    //     console.log(buildMeUp);
+    // }
 
 }
 
